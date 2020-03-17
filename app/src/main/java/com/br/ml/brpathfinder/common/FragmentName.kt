3df02c323e130c95b6ba7cfb890a8f70a8701935
BR-Sam <58094313+BR-Sam@users.noreply.github.com>
2020-03-16 20:02:48 -0500

    pr fix

--- app/src/main/java/com/br/ml/brpathfinder/common/FragmentName.kt
+++ app/src/main/java/com/br/ml/brpathfinder/common/FragmentName.kt
@@ -3,6 +3,5 @@ package com.br.ml.brpathfinder.common
 //add name of implementation fragment here
 
 enum class FragmentName {
-    AR_FRAGMENT, ML_FRAGMENT, DEPTH_FRAGMENT
-    AR_FRAGMENT, ML_FRAGMENT, SETTINGS
+    AR_FRAGMENT, ML_FRAGMENT, DEPTH_FRAGMENT, SETTINGS
 }
